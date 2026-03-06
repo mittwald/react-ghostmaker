ab3dd25fc1c812a2c08f354cd69ef7ee21b50de6
Marco Falkenberg <m.falkenberg@mittwald.de>
2026-03-06 09:51:01 +0100

    fix: do not use directory exports because of import issues

--- packages/react-ghostmaker/src/index.ts
+++ packages/react-ghostmaker/src/index.ts
@@ -3,6 +3,6 @@ export { registerModelIdentifier } from "./modelIdentifier.ts";
 export { type ReactGhost, UseGhostReturn } from "./types.ts";
 export { getQueryContext, forwardQueryContext } from "./context.ts";
 export { invalidateGhosts } from "./invalidate.ts";
-export * from "./maybeGhost";
+export * from "./maybeGhost/index.ts";
 export { GhostMakerModel, ghostMakerModel } from "./metaData.ts";
 export { getModelId, getModelName } from "./queries.ts";
